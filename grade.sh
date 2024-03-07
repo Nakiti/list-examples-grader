34ee9ad7d0eb921f42c411b370df77bf85aadf3d
Nakiti <nikhilakiti91@gmail.com>
2024-03-07 15:17:09 -0800

    Fixed bugs

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,44 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [ -f "student-submission/ListExamples.java" ]; then 
+    echo "file found!"
+else 
+    echo "ListExamples.java not found!"
+    exit 1
+fi
+
+#jar
+cp -r lib grading-area
+
+# ListExamples
+cp student-submission/ListExamples.java grading-area/
+
+#TestListExamples
+cp TestListExamples.java grading-area/
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]; then
+    echo "Complie error!"
+else
+    echo "Compiled successfully!"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -2)
+ok=$(grep -o "OK" junit-output.txt)
+
+if [[ "$ok" =~ "OK" ]]; then
+    tests=$(echo $lastline | awk -F'[ (]' '{print $3}')
+    echo "Your score is $tests / $tests "
+else 
+    tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+    failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+    successes=$(( tests - failures ))
+    echo "Your score is $successes / $tests"
+fi
+
